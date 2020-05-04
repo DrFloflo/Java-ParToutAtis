110d2b95cd9eb8dda2bc2c62960c60059b4d56d0
Philippe Maquin <philippemaquin2705@gmail.com>
2020-05-04 10:29:17 +0200

    Create Review.java
    BRETAGNE INDEPENDAAAAAAAAAAAAANTE

---
+++ src/opinion/Review.java
@@ -0,0 +1,18 @@
+package opinion;
+
+public class Review {
+
+    private String title;
+    private int date;
+    public String member;
+    private int note;
+    private String comment;
+
+    public Review(String title, int date, String member, int note, String comment) {
+        this.name = title;
+        this.date = date;
+        this.login = member;
+        this.pwd = note;
+        this.mail = comment;
+    }
+}
