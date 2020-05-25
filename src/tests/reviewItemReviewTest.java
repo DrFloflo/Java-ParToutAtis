7bceae6436738c89989407789cb7f30d9ab7ba1d
Philippe Maquin <philippemaquin2705@gmail.com>
2020-05-25 11:52:55 +0200

    Update reviewItemReviewTest.java

--- src/tests/reviewItemReviewTest.java
+++ src/tests/reviewItemReviewTest.java
@@ -476,17 +476,18 @@ public class reviewItemReviewTest {
 
         try {
             opinion.Film leFilm = sn.getFilm("The big Lebowski");
+            Review reviewTrouve = null;
+            for (Review eachReviewInFilm : leFilm.getReview()) {            //Verify if the film exists
+                if (eachReviewInFilm .getMember().equals("paul")) {
+                    reviewTrouve=eachReviewInFilm ;
+                }
+            }
         }
         catch (BadEntryException e){ //This shouldn't happen
             System.out.println("Unexpected error");
             return null;
         }
-        Review reviewTrouve = null;
-        for (Review eachReviewInFilm : leFilm.getReview) {            //Verify if the film exists
-            if (eachReviewInFilm .getLogin("paul") == "Pseudo utilisateur de la review que je commente") {
-                reviewTrouve=eachReviewInFilm ;
-            }
-        }
+
 
         System.out.println("Testing reviewItemReview()");
 
